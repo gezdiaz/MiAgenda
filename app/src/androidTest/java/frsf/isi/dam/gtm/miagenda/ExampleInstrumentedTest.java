febc5842594493ee0d0d513777aa97337c8253a3
Gaston <gastonezequieldiaz@outlook.com>
2019-12-19 19:37:22 -0300

    Proyecto creado

---
+++ app/src/androidTest/java/frsf/isi/dam/gtm/miagenda/ExampleInstrumentedTest.java
@@ -0,0 +1,25 @@
+package frsf.isi.dam.gtm.miagenda;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("frsf.isi.dam.gtm.miagenda", appContext.getPackageName());
+    }
+}
