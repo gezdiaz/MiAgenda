86f0e7e04daf26a34cafd0277d621012057453b9
Tomas Ravelli <travelli@frsf.utn.edu.ar>
2020-01-21 18:40:02 -0300

    Arreglos interfaz Login

--- app/src/main/java/frsf/isi/dam/gtm/miagenda/interfaces/LoginActivity.java
+++ app/src/main/java/frsf/isi/dam/gtm/miagenda/interfaces/LoginActivity.java
@@ -2,13 +2,11 @@ package frsf.isi.dam.gtm.miagenda.interfaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,35 +27,38 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_avtivity);
-        correoEdit = findViewById(R.id.loginUsuarioEdit);
-        contraseniaEdit = findViewById(R.id.loginPasswordEdit);
-        iniciarSesionBtn = findViewById(R.id.iniciarSesionBtn);
-        iniciarConGoogleBtn = findViewById(R.id.iniciarSesionGoogleBtn);
-        crearCuentaBtn = findViewById(R.id.crearCuentaBtn);
+        correoEdit = findViewById(R.id.login_usuario_edit);
+        contraseniaEdit = findViewById(R.id.login_password_edit);
+        iniciarSesionBtn = findViewById(R.id.iniciar_sesion_btn);
+        iniciarConGoogleBtn = findViewById(R.id.iniciar_sesion_google_btn);
+        crearCuentaBtn = findViewById(R.id.crear_cuenta_btn);
         iniciarSesionBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 correoEdit.clearFocus();
                 contraseniaEdit.clearFocus();
                 if(correoEdit.getText().toString().isEmpty() || contraseniaEdit.getText().toString().isEmpty()){
-
-                    //Tuve que hacer un Layout a parte para poder cambiar el color del fondo del toast ya que no se distinguia por el fondo blanco.
-                    //Ademas del layout a parte se creo tambien un drawable para poder agregar bordes redondeados a dicho Layout
-
-                    LayoutInflater inflater = getLayoutInflater();
-                    View layout = inflater.inflate(R.layout.toast_error_login,
-                            (ViewGroup) findViewById(R.id.linearLayout_toast));
-
-                    TextView text = layout.findViewById(R.id.errorTextLbl);
-                    text.setText(getString(R.string.datosLoginNoValidos));
-
-                    Toast toast = new Toast(getApplicationContext());
-                    toast.setGravity(Gravity.BOTTOM, 0, 50);
-                    toast.setDuration(Toast.LENGTH_LONG);
-                    toast.setView(layout);
-                    toast.show();
+                    mostrarToastError();
                 }
             }
         });
+    }
+
+    private void mostrarToastError(){
+        //Tuve que hacer un Layout a parte para poder cambiar el color del fondo del toast ya que no se distinguia por el fondo blanco.
+        //Ademas del layout a parte se creo tambien un drawable para poder agregar bordes redondeados a dicho Layout
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_error_login,
+                (ViewGroup) findViewById(R.id.linear_layout_toast));
+
+        TextView text = layout.findViewById(R.id.error_text_lbl);
+        text.setText(getString(R.string.datos_login_no_validos));
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.BOTTOM, 0, 50);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
     }
 }
