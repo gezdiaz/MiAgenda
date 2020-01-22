89334aa57888c39151fab4b7be3f9abfb1735a67
Gaston <gastonezequieldiaz@outlook.com>
2020-01-21 23:39:36 -0300

    Drawer funcionando
    Ya se puede usar el drawer para navegar entre las pantallas de Mi Agenda y Mis pacientes

---
+++ app/src/main/java/frsf/isi/dam/gtm/miagenda/interfaces/ui/gallery/MisPacientesFragment.java
@@ -0,0 +1,26 @@
+package frsf.isi.dam.gtm.miagenda.interfaces.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import frsf.isi.dam.gtm.miagenda.R;
+
+public class MisPacientesFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_mis_pacientes, container, false);
+        final TextView textView = root.findViewById(R.id.text_gallery);
+        textView.setText("This is Mis Pacientes fragment");
+        return root;
+    }
+}
---
+++ app/src/main/java/frsf/isi/dam/gtm/miagenda/interfaces/ui/home/MiAgendaFragment.java
@@ -0,0 +1,26 @@
+package frsf.isi.dam.gtm.miagenda.interfaces.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import frsf.isi.dam.gtm.miagenda.R;
+
+public class MiAgendaFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_mi_agenda, container, false);
+        return root;
+    }
+}
