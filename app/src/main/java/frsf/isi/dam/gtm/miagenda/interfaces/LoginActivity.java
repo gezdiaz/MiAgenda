ca2aa2066b547a6c3112d6a04aac8e945c236830
Gaston <gastonezequieldiaz@outlook.com>
2020-01-27 22:20:25 -0300

    Login empezado
    Ya se puede crear una cuenta e iniciar sesión con email y contraseña.
    El inicio de sesión con Google no funciona bien.

--- app/src/main/java/frsf/isi/dam/gtm/miagenda/interfaces/LoginActivity.java
+++ app/src/main/java/frsf/isi/dam/gtm/miagenda/interfaces/LoginActivity.java
@@ -1,35 +1,52 @@
 package frsf.isi.dam.gtm.miagenda.interfaces;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import frsf.isi.dam.gtm.miagenda.R;
 import frsf.isi.dam.gtm.miagenda.interfaces.drawerprincipal.PrincipalActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
+    private static final int RC_SIGN_IN_GOOGLE = -1;
     private TextInputEditText correoEdit;
-    private TextInputEditText contraseniaEdit;
+    private TextInputEditText claveEdit;
     private MaterialButton iniciarSesionBtn;
     private MaterialButton iniciarConGoogleBtn;
     private MaterialButton crearCuentaBtn;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_avtivity);
         correoEdit = findViewById(R.id.login_usuario_edit);
-        contraseniaEdit = findViewById(R.id.login_password_edit);
+        claveEdit = findViewById(R.id.login_password_edit);
         iniciarSesionBtn = findViewById(R.id.iniciar_sesion_btn);
         iniciarConGoogleBtn = findViewById(R.id.iniciar_sesion_google_btn);
         crearCuentaBtn = findViewById(R.id.crear_cuenta_btn);
@@ -37,16 +54,16 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 correoEdit.clearFocus();
-                contraseniaEdit.clearFocus();
-                if(correoEdit.getText().toString().isEmpty() || contraseniaEdit.getText().toString().isEmpty()){
-
+                claveEdit.clearFocus();
+                String email = correoEdit.getText().toString();
+                String clave = claveEdit.getText().toString();
+                if(email.isEmpty() || clave.isEmpty()){
                     Toast t = Toast.makeText(getApplicationContext(),getString(R.string.datos_login_no_validos), Toast.LENGTH_LONG);
                     t.show();
-                }
-                else{
+                }else{
                     Intent i1 = new Intent(getApplicationContext(), PrincipalActivity.class);
                     //TODO iniciar sesión con mail y contraseña
-                    startActivity(i1);
+                    iniciarSesion(email, clave);
                 }
             }
         });
@@ -54,10 +71,140 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //TODO iniciar sesión con google
+                iniciarSesionConGoogle();
+            }
+        });
+        crearCuentaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                correoEdit.clearFocus();
+                claveEdit.clearFocus();
+                if(correoEdit.getText().toString().isEmpty() || claveEdit.getText().toString().isEmpty()){
+
+                    Toast t = Toast.makeText(getApplicationContext(),getString(R.string.datos_login_no_validos), Toast.LENGTH_LONG);
+                    t.show();
+                }else{
+                    if(claveEdit.getText().length() > 6){
+                        Toast.makeText(LoginActivity.this, R.string.error_clave_6_caracteres, Toast.LENGTH_LONG).show();
+                    }else{
+                        crearCuenta(correoEdit.getText().toString(), claveEdit.getText().toString());
+                    }
+                }
+            }
+        });
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .requestProfile()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+//        FirebaseUser user = mAuth.getCurrentUser();
+//        if(user != null){
+//            Log.d(TAG, "Signing out. UserEmail: "+user.getEmail());
+//            mAuth.signOut();
+//        }
+
+    }
+
+    private void iniciarSesionConGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        Log.d(TAG, "Llama startActivityForResult");
+        startActivityForResult(signInIntent, RC_SIGN_IN_GOOGLE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult");
+        if(requestCode == RC_SIGN_IN_GOOGLE){
+            Log.d(TAG, "Entra al if");
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "Llama a firebaseAuthWhitGoogle()");
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                Toast.makeText(this, R.string.error_inicio_sesion_google, Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    // Sign in success
+                    Log.d(TAG, "signInWithCredential:success");
+                    Toast.makeText(LoginActivity.this, R.string.exito_inicio_sesion, Toast.LENGTH_LONG).show();
+//                    Snackbar.make(findViewById(R.id.login_activity_layout), R.string.exito_inicio_sesion, Snackbar.LENGTH_LONG).show();
+                    startActivity(new Intent(LoginActivity.this, PrincipalActivity.class));
+                    finish();
+                }else{
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.getException());
+                    Toast.makeText(LoginActivity.this, R.string.error_inicio_sesion_google, Toast.LENGTH_LONG).show();
+//                    Snackbar.make(findViewById(R.id.login_activity_layout), R.string.error_inicio_sesion_google, Snackbar.LENGTH_LONG).show();
+                }
             }
         });
 
     }
 
+    private void iniciarSesion(String email, String clave) {
+        mAuth.signInWithEmailAndPassword(email, clave)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            Toast.makeText(LoginActivity.this, getString(R.string.exito_inicio_sesion), Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(LoginActivity.this, PrincipalActivity.class));
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, R.string.error_inicio_sesion, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void crearCuenta(String email, String clave) {
+
+        mAuth.createUserWithEmailAndPassword(email, clave)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(LoginActivity.this, getString(R.string.exito_creacion_cuenta), Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(LoginActivity.this, PrincipalActivity.class));
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, getString(R.string.fallo_creacion_cuenta), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
 
 }
