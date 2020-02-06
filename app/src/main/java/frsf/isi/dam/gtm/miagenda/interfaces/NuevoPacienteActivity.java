b0a504ae3e408368f1995bd0b2a1e0d2a2c18b4e
Tomas Ravelli <travelli@frsf.utn.edu.ar>
2020-02-06 19:57:48 -0300

    Agregado de nuevo de la funcion con la camara.

--- app/src/main/java/frsf/isi/dam/gtm/miagenda/interfaces/NuevoPacienteActivity.java
+++ app/src/main/java/frsf/isi/dam/gtm/miagenda/interfaces/NuevoPacienteActivity.java
@@ -1,13 +1,19 @@
 package frsf.isi.dam.gtm.miagenda.interfaces;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
@@ -22,17 +28,25 @@ import java.util.TimeZone;
 import frsf.isi.dam.gtm.miagenda.R;
 
 public class NuevoPacienteActivity extends AppCompatActivity {
-    //TODO ver los de la camara
+
+    private final int REQUEST_IMAGE_CAPTURE = 1;
+
     private Toolbar myToolBar;
     private Calendar fechaNacimiento;
+    private RelativeLayout fotoPacienteRelativeLayout;
     private TextInputEditText nombrePacienteEdit, apellidoPacienteEdit, fechaNacimientoEdit,dniEdit, telefonoEdit, provinciaEdit, ciudadEdit, calleEdit, numeroDireccionEdit,obraSocialEdit;
-    private MaterialButton tomarFotoBtn, registrarPacBtn;
+    private MaterialButton tomarFotoBtn, registrarPacBtn, eliminarFotoBtn;
+    private ImageView fotoPacienteImageView;
     private  boolean[] validaciones = {false,false,false,false,false,false,false,false,false,false};
     private boolean datosValidos = true;
     private int contadorWhile = 0;
 
     private MaterialDatePicker.Builder<Long> builder;
     private MaterialDatePicker<Long> datePicker;
+
+    private  Bitmap imageBitmap;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,8 +69,11 @@ public class NuevoPacienteActivity extends AppCompatActivity {
         calleEdit = findViewById(R.id.calle_edit);
         numeroDireccionEdit  = findViewById(R.id.numero_direccion_edit);
         obraSocialEdit = findViewById(R.id.obra_social_edit);
+        fotoPacienteImageView = findViewById(R.id.foto_paciente_image_view);
         tomarFotoBtn = findViewById(R.id.tomar_foto_btn);
         registrarPacBtn = findViewById(R.id.registrar_paciente_btn);
+        fotoPacienteRelativeLayout = findViewById(R.id.image_view_paciente_relative_layout);
+        eliminarFotoBtn = findViewById(R.id.eliminar_foto_btn);
 
         fechaNacimientoEdit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -226,6 +243,27 @@ public class NuevoPacienteActivity extends AppCompatActivity {
 
             }
         });
+
+
+        tomarFotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+
+
+            }
+        });
+
+        eliminarFotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fotoPacienteImageView.setImageBitmap(null);
+                fotoPacienteRelativeLayout.setVisibility(View.GONE);
+            }
+        });
     }
 
     @Override
@@ -270,5 +308,16 @@ public class NuevoPacienteActivity extends AppCompatActivity {
                 fechaNacimientoEdit.setText(fechaSeleccionada);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            fotoPacienteImageView.setImageBitmap(imageBitmap);
+            fotoPacienteRelativeLayout.setVisibility(View.VISIBLE);
+        }
     }
 }
